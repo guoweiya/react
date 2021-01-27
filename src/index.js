97ddcc93521dfece3d631e2d9396c96795047883
guopengwei <653015681@qq.com>
2021-01-27 13:56:40 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// function greeting(name) {
+//   if (name) {
+//     return <h1>Hello, { name }!</h1>
+//   }
+//   return <h1>Hello, Stranger.</h1>
+// }
+// let root = greeting('')
+
+// let names = ['张三', '李四', '王五']
+// let elements = []
+// for (let i = 0; i < names.length; i++) {
+//   elements.push(<li>{ names[i] }</li>)
+// }
+
+function tick() {
+  const element = (
+    <div>
+      { new Date().toLocaleDateString() }
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <h1>root</h1>,
+  tick,
   document.getElementById('root')
 );
